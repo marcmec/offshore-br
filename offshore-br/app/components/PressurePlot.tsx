268c6c7e3f9373694254aef62c4cf3682113f367
marcos vinicius <viniciuslag@gmail.com>
2023-11-17 17:45:59 -0300

    fix:edit plot

--- offshore-br/app/components/PressurePlot.tsx
+++ offshore-br/app/components/PressurePlot.tsx
@@ -1,34 +1,52 @@
-import { Gauge } from "@ant-design/plots";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { Gauge } from '@ant-design/plots';
 
 const PressurePlot = () => {
+  const [pressure,setPressure]= useState(15.4)
   const config = {
-    percent: 0.75,
+    radius:1,
+    percent: pressure*0.01,
+    innerRadius: 0.9,
     range: {
-      color: 'l(0) 0:#B8E1FF 1:#ff0000',
+      color: ['#30BF78', '#ff0000','' ],
     },
-    startAngle: Math.PI,
-    endAngle: 2 * Math.PI,
-    indicator: null,
-    statistic: {
-      title: {
-        offsetY: -36,
-        style: {
-          fontSize: '24px',
-          color: '#4B535E',
+    axis: {
+      label: {
+        formatter(v:any) {
+          return Number(v) * 100;
         },
-        formatter: () => '70Pa',
       },
-      // content: {
-      //   style: {
-      //     fontSize: '24px',
-      //     lineHeight: '44px',
-      //     color: '#4B535E',
-      //   },
-      //   formatter: () => '',
-      // },
+      subTickLine: {
+        count: 5,
+      },
+    },
+    indicator: {
+      pointer: {
+        style: {
+          stroke: 'red',
+
+        },
+      },
+      pin: {
+        style: {
+          stroke: 'black',
+        },
+        
+      },
+    },
+    statistic: {
+      content: {
+        formatter:()=>`${pressure}Pa`,
+        style: {
+          fontSize: '12px',
+          lineHeight: '12px',
+          color:"black"
+        },
+      },
     },
   };
-  return <Gauge {...config} height={100}/>;
+  return <Gauge {...config} height={100} />;
 };
 
 export default PressurePlot
